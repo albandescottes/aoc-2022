3ce91e642ab704b109e5c75b3d9c15a608c08876
a.descottes@groupeonepoint.com <a.descottes@groupeonepoint.com>
2022-12-07 14:44:12 +0100

    day 7 refactor

--- src/day7/index.ts
+++ src/day7/index.ts
@@ -4,20 +4,16 @@ interface Dir {
   i: number;
   n: string;
   p?: number;
-  f: number[];
+  fileSize: number;
   d: number[];
   s?: number;
 }
 
-interface File {
-  i: number;
-  n: string;
-  s: number;
-}
-
 type mapDirs = { [k: number]: Dir };
-type mapFiles = { [k: number]: File };
 
+// après test, il n'y a jamais deux fois ls dans le même dir
+// donc on peut ommetre la vérificaiton avant création
+// et ne pas avoir un mapping de fichiers
 class Day7 extends Day {
   uuidv1 = 1;
   constructor() {
@@ -25,16 +21,11 @@ class Day7 extends Day {
   }
 
   private initEmptyDir(i: number, n: string, p: number): Dir {
-    return { i, n, p, f: [], d: [] };
+    return { i, n, p, fileSize: 0, d: [] };
   }
 
-  private initNewFile(i: number, n: string, s: number): File {
-    return { i, n, s };
-  }
-
-  private parseData(data: string[]): { dirs: mapDirs; files: mapFiles } {
-    const dirs: mapDirs = { 0: { i: 0, n: "/", f: [], d: [] } };
-    const files: mapFiles = {};
+  private parseData(data: string[]): mapDirs {
+    const dirs: mapDirs = { 0: { i: 0, n: "/", fileSize: 0, d: [] } };
     let curr = dirs[0];
     let mode: "cd" | "ls";
     data.forEach((s) => {
@@ -49,69 +40,57 @@ class Day7 extends Day {
           const idx = dirs[curr.i].d.find((i) => dirs[i]?.n === path);
           if (idx !== undefined) curr = dirs[idx];
         }
-        // console.log('curr', curr.n, curr.d);
       } else if (split[1] == "ls") mode = "ls";
-      // si pas ls/cd et mode ls
       else if (mode === "ls") {
         const [type, name] = split;
-        // console.log("trying to add", type, name, "in", curr.n);
         if (type === "dir") {
-          const idx = dirs[curr.i].d.find((i) => dirs[i]?.n === name);
-          if (idx === undefined) {
-            const nd = this.initEmptyDir(this.uuidv1++, name, curr.i);
-            dirs[curr.i] = { ...dirs[curr.i], d: [...dirs[curr.i].d, nd.i] };
-            dirs[nd.i] = nd;
-          }
+          const nd = this.initEmptyDir(this.uuidv1++, name, curr.i);
+          dirs[curr.i] = { ...dirs[curr.i], d: [...dirs[curr.i].d, nd.i] };
+          dirs[nd.i] = nd;
         } else {
-          const idx = dirs[curr.i].f.find((i) => files[i]?.n === name);
-          if (idx === undefined) {
-            const nd = this.initNewFile(this.uuidv1++, name, +type);
-            dirs[curr.i] = { ...dirs[curr.i], f: [...dirs[curr.i].f, nd.i] };
-            files[nd.i] = nd;
-          }
+          const fileSize = dirs[curr.i].fileSize + +type;
+          dirs[curr.i] = { ...dirs[curr.i], fileSize };
         }
       }
     });
-
-    return {
-      dirs,
-      files,
-    };
+    Object.keys(dirs).forEach((k) => {
+      dirs[+k] = { ...dirs[+k], s: this.computeSize(dirs, +k) };
+    });
+    return dirs;
   }
 
-  private computeSize(dirs: mapDirs, files: mapFiles, idx: number): number {
+  private computeSize(dirs: mapDirs, idx: number): number {
     const curr = dirs[idx];
-    const totFiles = curr.f.map((i) => files[i].s).reduce((a, b) => a + b, 0);
-    const totDirs = curr.d.map((i) => {
-      return this.computeSize(dirs, files, i);
-    }).reduce((a, b) => a + b, 0);
-    return totFiles + totDirs;
+    const totDirs = curr.d
+      .map((i) => {
+        return this.computeSize(dirs, i);
+      })
+      .reduce((a, b) => a + b, 0);
+    return curr.fileSize + totDirs;
   }
 
   solveForPartOne(input: string): string {
     const data = input.split("\n");
-    const { dirs, files } = this.parseData(data);
-    Object.keys(dirs).forEach((k) => {
-        dirs[+k] = {...dirs[+k], s: this.computeSize(dirs, files, +k)}});
-    const lighweightDirs = Object.keys(dirs)
-    .map(k => dirs[+k].s || 0)
-    .filter(s => s < 100000);
-    return `${lighweightDirs.reduce((a,b) => a+b)}`;
+    const dirs = this.parseData(data);
+    const tH = 1e5;
+    const sumSubTH = Object.keys(dirs)
+      .map((k) => dirs[+k].s || 0)
+      .filter((s) => s < tH)
+      .reduce((a, b) => a + b);
+    return `${sumSubTH}`;
   }
 
   solveForPartTwo(input: string): string {
     const data = input.split("\n");
-    const { dirs, files } = this.parseData(data);
-    Object.keys(dirs).forEach((k) => {
-        dirs[+k] = {...dirs[+k], s: this.computeSize(dirs, files, +k)}});
-
+    const dirs = this.parseData(data);
     const total = dirs[0].s || 0;
     const dirsByWeigth = Object.keys(dirs)
-    .map(k => ({ dir: +k, w: (dirs[+k].s || 0)}))
-    .sort((a,b) => b.w - a.w);
-    const firstNotEnough = dirsByWeigth
-    .findIndex((v) => ((30000000 - (70000000 - total)) - v.w) > 0)
-    return `${dirsByWeigth[firstNotEnough-1].w}`
+      .map((k) => dirs[+k].s || 0)
+      .sort((a, b) => b - a);
+    const firstNotEnough = dirsByWeigth.findIndex(
+      (v) => total - 4e7 - v > 0
+    );
+    return `${dirsByWeigth[firstNotEnough - 1]}`;
   }
 }
 
